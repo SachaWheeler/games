6463f9e1147d0c80c83c45abfd5d457a0b890ad7
Sacha Wheeler <sacha@sachawheeler.com>
2024-10-28 07:41:07 +0000

    save point

--- cross_grid.py
+++ cross_grid.py
@@ -5,7 +5,7 @@ import nltk
 import itertools
 
 # nltk.download('words')
-WORD_LENGTH = 5
+WORD_LENGTH = 3
 FILENAME="words_alpha_3.txt"
 
 def get_word_list(filename=FILENAME):
@@ -33,12 +33,13 @@ def create_word_grid(first_row, middle_row, last_row):
                 grid = [first] +  list(middle) + [last]
                 columns = ["".join(row[i] for row in grid) for i in range(WORD_LENGTH)]
 
-                if count%10000000==0:
+                if count%100000==0:
                     print(f"{count}: {grid=}")
                 count += 1
                 # print(f"{columns=}")
                 # no common elements and columns are legitimate words
-                if not set(grid).intersection(columns) and all(col in all_words for col in columns):
+                if not set(grid).intersection(columns) and \
+                        all(col in all_words for col in columns):
                     return grid
 
     return None
@@ -51,7 +52,7 @@ def display_grid(grid):
             print(" ".join(row))
         print("\nColumns:")
         for i in range(WORD_LENGTH):
-            print("".join(row[i] for row in grid))
+            print(" ".join(row[i] for row in grid))
     else:
         print("No valid grid found.")
 
